8ceab2182e1fc41c81a9ff4a69981a1b5b3a36e5
Eugenia <peregoodovaeva@gmail.com>
2023-06-16 21:16:40 +0300

    feat: args.js is done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+    const cliArgs = process.argv.slice(2)
+    let answer = ''
+
+    for (let i = 0; i < cliArgs.length; i++) {
+        if (cliArgs[i].startsWith('--')) {
+            answer += `${cliArgs[i].slice(2)} is ${cliArgs[i + 1]}, `
+        }
+    }
+
+    console.log(answer.slice(0, answer.length - 2))
 };
 
 parseArgs();
