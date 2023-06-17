66afd7bf53a9448d05b589360ac8a7e48650ebd9
Eugenia <peregoodovaeva@gmail.com>
2023-06-17 19:57:16 +0300

    feat: copy.js is done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+
 const copy = async () => {
-    // Write your code here 
+  const dirPath = './files'
+  const newDirPath = './files_copy'
+
+  try {
+    await fs.promises.access(dirPath)
+    await fs.promises.mkdir(newDirPath)
+  } catch (error) {
+    throw new Error('FS operation failed')
+  }
+
+    await fs.promises.cp(dirPath, newDirPath, {recursive: true})
 };
 
 await copy();
