534a86df71f58e43772a418c5ed3723a519625bb
Eugenia <peregoodovaeva@gmail.com>
2023-06-17 20:58:32 +0300

    feat: rename.js is done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import fs from "fs";
+
+async function fileCheck(path) {
+  try {
+    await fs.promises.access(path)
+    return true
+  } catch(e) {
+    return false
+  }
+}
+
 const rename = async () => {
-    // Write your code here 
+  const oldPath = './files/wrongFilename.txt'
+  const newPath = './files/wrongFilename.txt'
+
+  if (!(await fileCheck(oldPath)) || await fileCheck(newPath)) {
+    throw new Error('FS operation failed')
+  }
+
+  await fs.promises.rename(oldPath, newPath)
 };
 
 await rename();
