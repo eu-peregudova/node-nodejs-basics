877c0f902cd8f7a8c7b78247a90ea4a2d803b256
Eugenia <peregoodovaeva@gmail.com>
2023-06-18 01:25:00 +0300

    feat: calcHash.js is done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+const { createHash } = await import('node:crypto');
+
 const calculateHash = async () => {
-    // Write your code here 
+  let fileData;
+  const hash = createHash('sha256')
+
+  try {
+    fileData = await fs.promises.readFile('./files/fileToCalculateHashFor.txt')
+  } catch (error) {
+    throw new Error(error)
+  }
+
+  hash.update(fileData)
+
+  console.log(`${hash.digest('hex')}`)
 };
 
 await calculateHash();
