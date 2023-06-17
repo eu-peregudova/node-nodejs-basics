752b60e0d8a7f6ded103d28dc5c3dab5167254a8
Eugenia <peregoodovaeva@gmail.com>
2023-06-17 21:11:03 +0300

    feat: list.js is done

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,10 @@
+import fs from "fs";
+
 const list = async () => {
-    // Write your code here 
+  const path = './files'
+  const result = await fs.promises.readdir(path)
+
+  console.log(result.join('; '))
 };
 
 await list();
